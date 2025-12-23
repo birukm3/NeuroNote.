08951074b24fdb602584cdf6424d4e87b327d1b5
Biruk Molla <birukm@Biruks-MacBook-Pro.local>
2025-12-23 02:44:53 -0500

    render config

--- backend/app/app.py
+++ backend/app/app.py
@@ -95,4 +95,5 @@ def get_flashcards(user_id):
 
 
 if __name__ == '__main__':
-    app.run(port=5050, debug=True)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=False)
